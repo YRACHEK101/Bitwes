4f6ba4e9ac085b0e9cbbecfc84c8d2ce82900a88
YRACHEK101 <r.yahia.dev@gmail.com>
2025-03-01 07:24:26 +0000

    update news.json

---
+++ .next/static/webpack/app/page.2514543e3ffba4da.hot-update.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("app/page",{
+
+/***/ "(app-pages-browser)/./json/news.json":
+/*!************************!*\
+  !*** ./json/news.json ***!
+  \************************/
+/***/ (function(module, __unused_webpack_exports, __webpack_require__) {
+
+module.exports = JSON.parse('["Oxfam fait état d\'une accélération du creusement des inégalités","Modèle allemand en crise : vers la fin de la rigueur ?","Allemagne : menace sur l\'emploi","Google Maps fête ses 20 ans : l\'application qui redessine le monde","L\'intelligence artificielle, moteur ou frein de l\'activité boursière?"]');
+
+/***/ })
+
+});
---
+++ .next/static/webpack/app/page.794f01249e9b5cef.hot-update.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("app/page",{
+
+/***/ "(app-pages-browser)/./json/news.json":
+/*!************************!*\
+  !*** ./json/news.json ***!
+  \************************/
+/***/ (function(module, __unused_webpack_exports, __webpack_require__) {
+
+module.exports = JSON.parse('["Christopher Vaccaro reports on the latest street art festival","A look back at the events which defined Berlin\'s cultural landscape","Episode 333 of Fyrre Magazine\'s podcast series is out now"]');
+
+/***/ })
+
+});
